16132ca5e694519cae6f7c45dd543f0b63d6a2cc
romain <romain.feron@evobio.eu>
2016-04-25 09:59:00 +0200

    timer

--- src/dna_to_bin.cpp
+++ src/dna_to_bin.cpp
@@ -3,13 +3,17 @@
 #include <bitset>
 #include <fstream>
 #include <iostream>
+#include <chrono>
 
 
 int main(int argc, char *argv[])
 {
 
+    std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
     std::ifstream f;
     f.open("reads1.fasta");
+
+    std::cout<<std::endl;
 
     std::string line;
 
@@ -29,6 +33,13 @@ int main(int argc, char *argv[])
 
     f.close();
 
+    std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+
+    duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+    std::cout << " - File 1 loaded (" << duration << " µs)" << std::endl;
+
+    t1 = std::chrono::high_resolution_clock::now();
+
     f.open("reads2.fasta");
     std::vector<std::bitset<2*s>> reads_2;
 
@@ -43,6 +54,13 @@ int main(int argc, char *argv[])
     }
 
     f.close();
+
+    t2 = std::chrono::high_resolution_clock::now();
+
+    duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+    std::cout << " - File 2 loaded (" << duration << " µs)" << std::endl;
+
+    t1 = std::chrono::high_resolution_clock::now();
 
     uint a=0, b=0, c=0;
     for (uint i=0; i<reads_1.size(); ++i){
@@ -61,6 +79,11 @@ int main(int argc, char *argv[])
 
         ++c;
     }
+
+    t2 = std::chrono::high_resolution_clock::now();
+
+    duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+    std::cout << " - Comparison ended (" << duration << " µs)" << std::endl;
 
     std::cout << std::endl;
     std::cout << "Equal : " << a << std::endl;
